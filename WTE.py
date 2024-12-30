fdae3f97e57501c91649e8d9cb0d4353b306320a
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 18:42:54 -0500

    ready and choice

--- WTE.py
+++ WTE.py
@@ -1,10 +1,11 @@
 #Ideas: choose a fighter, D&D-style battles (short, 1v1 or 2v1)
+import random
 
 print("WHERE TO EAT V1")
 
 #Asks how many options and displays integer answer
 numOptions = int(input("How many options do you want to have? "))
-print(f"You chose {numOptions} options.")
+print(f"You chose {numOptions} options. Input your spots.")
 
 #Asks for option names and displays list in uppercase
 options = [input(f"Option {i+1}: ") for i in range(numOptions)]
@@ -18,4 +19,13 @@ def duplicates(list):
 if duplicates(options):
     print("\033[91mSome spots have a greater chance than others.\033[0m")
 else:
-    print("All spots have an equal chance.")
+    print("All spots have an equal chance!")
+
+choice = random.choice(options)
+
+ready = str(input("Are you ready for your random choice? Y/N "))
+ready = ready.lower()
+if ready in ["y", "yes"]:
+    print(f"Head to {choice}!")
+else:
+    print("Come back soon!")
