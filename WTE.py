f9c546428d395aaf92d1f83c5ae2452f0f844f36
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 12:35:47 -0500

    Initial commit

---
+++ WTE.py
@@ -0,0 +1,8 @@
+print("WHERE TO EAT V1")
+
+option1 = input("Option 1: ")
+option2 = input("Option 2: ")
+option3 = input("Option 3: ")
+
+print("Your options are:", option1, option2, option3)
+print("You are going to:", option2)
