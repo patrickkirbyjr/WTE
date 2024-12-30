54f4331b5531a450b9db67a2e8042e9444ffe305
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 12:47:00 -0500

    added equal chance printout

--- WTE.py
+++ WTE.py
@@ -7,4 +7,10 @@ option2 = input("Option 2: ")
 option3 = input("Option 3: ")
 
 print("Your options are:", option1, option2, option3)
+
+if option1 == option2 == option3:
+    print("All spots have an equal chance.")
+else:
+    print("Some spots have a greater chance than others.")
+
 print("You are going to:", option2)
