f0e2f9cd49659db5d78ae5da32b57960203a8ce2
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 16:51:14 -0500

    use list of options

--- WTE.py
+++ WTE.py
@@ -2,27 +2,18 @@
 
 print("WHERE TO EAT V1")
 
-print("Choose three potential spots.")
+#Asks how many options and displays integer answer
+numOptions = int(input("How many options do you want to have? "))
+print(f"You chose {numOptions} options.")
 
-option1 = input("Option 1: ")
-option1 = option1.upper()
-option2 = input("Option 2: ")
-option2 = option2.upper()
-option3 = input("Option 3: ")
-option3 = option3.upper()
+#Asks for option names and displays list in uppercase
+options = [input(f"Option {i+1}: ") for i in range(numOptions)]
+options = [s.upper() for s in options]
+print("Your options are:", options)
 
-print("Your options are:", option1, option2, option3)
 
-if option1 != option2 != option3:
-    print("All spots have an equal chance.")
-else:
-    print("Some spots have a greater chance than others.")
 
-ready = str(input("Do you want me to decide? Y/N "))
-ready = ready.lower()
-
-if ready in ["y", "yes"]:
-    print("You are going to:", option2)
-else:
-    print("Come back soon!")
-
+#if option1 != option2 != option3:
+#    print("All spots have an equal chance.")
+#else:
+#    print("Some spots have a greater chance than others.")
