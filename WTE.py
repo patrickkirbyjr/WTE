24ae75e52c5399936a53beea281d785cde315209
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 16:27:01 -0500

    ideas, uppercase, lowercase

--- WTE.py
+++ WTE.py
@@ -1,10 +1,15 @@
+#Ideas: choose a fighter, D&D-style battles (short, 1v1 or 2v1)
+
 print("WHERE TO EAT V1")
 
-print("Choose three potential spots!")
+print("Choose three potential spots.")
 
 option1 = input("Option 1: ")
+option1 = option1.upper()
 option2 = input("Option 2: ")
+option2 = option2.upper()
 option3 = input("Option 3: ")
+option3 = option3.upper()
 
 print("Your options are:", option1, option2, option3)
 
@@ -14,8 +19,9 @@ else:
     print("Some spots have a greater chance than others.")
 
 ready = str(input("Do you want me to decide? Y/N "))
+ready = ready.lower()
 
-if ready in ["Y", "y", "yes", "Yes", "YES", "YEs", "yEs", "yES", "yeS", "YeS"]:
+if ready in ["y", "yes"]:
     print("You are going to:", option2)
 else:
     print("Come back soon!")
