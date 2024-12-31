068f14cf299a21bae0337b845a52dffd3820eb39
Patrick Kirby Jr. <patrickkirbyjr@outlook.com>
2024-12-30 19:45:17 -0500

    elimination!!!

--- WTE.py
+++ WTE.py
@@ -3,6 +3,7 @@ import random
 from collections import Counter
 
 print("WHERE TO EAT V1")
+print("")
 
 #Asks how many options and displays integer answer
 numOptions = int(input("How many options do you want to have? "))
@@ -11,28 +12,48 @@ print(f"You chose {numOptions} options. Input your spots.")
 #Asks for option names and displays list in uppercase
 options = [input(f"Option {i+1}: ") for i in range(numOptions)]
 options = [s.upper() for s in options]
+print("")
 #print("Your options are:", *options) #prints out a list
 
-print("Your options are: ")
-counts = Counter(options)
-for value, count in counts.items():
-    print(f"{value}: {count} count ({count/numOptions*100}% base chance)") #prints out counts of each place
-
+def display_options(options, numOptions):
+    print("Your options are: ")
+    counts = Counter(options)
+    for value, count in counts.items():
+        print(f"{value}: {count} count ({round(count/numOptions*100, 2)}% base chance)") #prints out counts of each place
 
 #Checks for duplicates and lets user know that some spots have a higher chance of being selected than others
-def duplicates(list):
-    return len(list) != len(set(list))
+def check_duplicates(options):
+    def duplicates(list):
+        return len(list) != len(set(list))
+    if duplicates(options):
+        print("\033[91mSome spots have a greater chance than others.\033[0m")
+    else:
+        print("All spots have an equal chance!")
 
-if duplicates(options):
-    print("\033[91mSome spots have a greater chance than others.\033[0m")
-else:
-    print("All spots have an equal chance!")
+#Eliminate one choice
+def remove_one_random(lst, num):
+    if not lst:
+        return None
+    else:
+        index = random.randint(0, len(lst) -1)
+        num -= 1
+        return lst.pop(index)
 
-choice = random.choice(options)
+def process():
+    display_options(options, numOptions)
+    check_duplicates(options)
+    print("")
 
-ready = str(input("Are you ready for your random choice? Y/N "))
-ready = ready.lower()
-if ready in ["y", "yes"]:
-    print(f"Head to \033[34m{choice}!\033[0m")
-else:
-    print("Come back soon!")
+process()
+
+while len(options) > 1:
+    ready = str(input("Are you ready to eliminate one option? Y/N "))
+    print("")
+    ready = ready.lower()
+    if ready in ["y", "yes"]:
+        remove_one_random(options, numOptions)
+        process()
+    else:
+        print(options)
+
+print(f"Head to \033[91m{options[-1]}!\033[0m")
